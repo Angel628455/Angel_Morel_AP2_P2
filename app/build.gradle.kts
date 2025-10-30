43099e87abb46f4d6498b739fb2d38c65bb0b74e
Angel Miguel Morel Rosario <angel_morel@ucne.edu.do>
2025-10-29 20:41:14 -0400

    Configurado Librerias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,29 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("androidx.room")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
-    namespace = "ucne.edu.angel_morel_ap2_p2"
+    namespace = "ucne.edu.angel_Morel_AP2_P2"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "ucne.edu.angel_morel_ap2_p2"
+        applicationId = "ucne.edu.angel_Morel_AP2_P2"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    room {
+        schemaDirectory("$projectDir/schemas")
     }
 
     buildTypes {
@@ -40,6 +48,22 @@ android {
 }
 
 dependencies {
+
+    //navegacion
+    implementation("androidx.navigation:navigation-compose:2.8.0-rc01")
+    implementation(libs.kotlin.serialization.json)
+
+    //room
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    //  optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:2.6.1")
+
+    //Hilt
+    implementation("com.google.dagger:hilt-android:2.51")
+    ksp("com.google.dagger:hilt-android-compiler:2.51")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
